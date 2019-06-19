ee41b8df408836cfd95aa9c7efe12f4baaad27a1
Christoph Powazny <christoph_powazny@gmx.de>
2019-06-19 20:42:10 +0200

    Set up Critic model based on the "Addressing Function Approximation Error in Actor-Critic Methods" paper -> C. DDPG Network and Hyper-parameter Comparison

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 
 
 class Actor(nn.Module):
@@ -19,3 +20,22 @@ class Actor(nn.Module):
 
     def forward(self, state):
         return self.max_action * self.network(state)
+
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super().__init__()
+        self.network = self._create_network(state_dim, action_dim)
+
+    @staticmethod
+    def _create_network(state_dim, action_dim):
+        return nn.Sequential(
+            nn.Linear(state_dim + action_dim, 400),
+            nn.ReLU(),
+            nn.Linear(400, 300),
+            nn.ReLU(),
+            nn.Linear(300, 1))
+
+    def forward(self, state, action):
+        state_and_action = torch.cat([state, action], 1)
+        return self.network(state_and_action)
